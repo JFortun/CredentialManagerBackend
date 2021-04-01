55feb48de2cd17589b3ec9e6496224c2bd61bf0f
JFortun <josemanuelfortunromero@gmail.com>
2021-04-01 11:36:16 +0200

    Initial commit

---
+++ src/main/java/com/fortun/credmanback/CredmanbackApplication.java
@@ -0,0 +1,13 @@
+package com.fortun.credmanback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredmanbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredmanbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fortun/credmanback/CredmanbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortun.credmanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredmanbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
