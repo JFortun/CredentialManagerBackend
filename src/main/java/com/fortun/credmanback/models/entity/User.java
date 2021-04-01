0637482b72f0b92782c743a278ba5a75aeafac50
JFortun <josemanuelfortunromero@gmail.com>
2021-04-01 12:33:50 +0200

    Implemented User entity

---
+++ src/main/java/com/fortun/credmanback/models/entity/User.java
@@ -0,0 +1,46 @@
+package com.fortun.credmanback.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long idUser;
+
+    @Column(name = "name_user")
+    private String nameUser;
+
+    @Column(name = "password_user")
+    private String passwordUser;
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getPasswordUser() {
+        return passwordUser;
+    }
+
+    public void setPasswordUser(String passwordUser) {
+        this.passwordUser = passwordUser;
+    }
+}
