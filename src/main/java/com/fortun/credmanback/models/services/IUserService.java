864ec85c9c3712ed64c02005a228a17e73533047
JFortun <josemanuelfortunromero@gmail.com>
2021-04-01 12:42:21 +0200

    Implemented User Service Interface

---
+++ src/main/java/com/fortun/credmanback/models/services/IUserService.java
@@ -0,0 +1,11 @@
+package com.fortun.credmanback.models.services;
+
+import com.fortun.credmanback.models.entity.User;
+
+import java.util.List;
+
+public interface IUserService {
+
+    public List<User> findAll();
+
+}
