721f25919a3ebd7a032978e08ec3f9994e516f6a
JFortun <josemanuelfortunromero@gmail.com>
2021-04-03 13:46:41 +0200

    Updated UserRestController with CRUD

--- src/main/java/com/fortun/credmanback/controllers/UserRestController.java
+++ src/main/java/com/fortun/credmanback/controllers/UserRestController.java
@@ -3,10 +3,8 @@ package com.fortun.credmanback.controllers;
 import com.fortun.credmanback.models.entity.User;
 import com.fortun.credmanback.models.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +17,35 @@ public class UserRestController {
     private IUserService userService;
 
     @GetMapping("/users")
-    public List<User> index(){
+    public List<User> index() {
         return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User show(@PathVariable Long id) {
+        return this.userService.findById(id);
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@ModelAttribute User user) {
+        this.userService.save(user);
+        return user;
+    }
+
+    @PutMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User update(@ModelAttribute User user, @PathVariable Long id) {
+        User currentUser = this.userService.findById(id);
+        currentUser.setNameUser(user.getNameUser());
+        currentUser.setPasswordUser(user.getPasswordUser());
+        this.userService.save(currentUser);
+        return currentUser;
+    }
+
+    @DeleteMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        this.userService.delete(id);
     }
 }
