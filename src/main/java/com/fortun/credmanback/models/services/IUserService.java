5fff8b03f9c79420375086ca69449c6d4fb0f556
JFortun <josemanuelfortunromero@gmail.com>
2021-04-03 13:35:49 +0200

    Updated IUserService with save, findById and delete

--- src/main/java/com/fortun/credmanback/models/services/IUserService.java
+++ src/main/java/com/fortun/credmanback/models/services/IUserService.java
@@ -8,4 +8,10 @@ public interface IUserService {
 
     public List<User> findAll();
 
+    public User save(User user);
+
+    public User findById(Long id);
+
+    public void delete(Long id);
+
 }
