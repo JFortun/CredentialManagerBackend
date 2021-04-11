66677ef40c5817a8864eed397942af9703e85675
JFortun <josemanuelfortunromero@gmail.com>
2021-04-11 12:57:42 +0200

    Implemented Credential Rest Controller

---
+++ src/main/java/com/fortun/credmanback/controllers/CredentialRestController.java
@@ -0,0 +1,53 @@
+package com.fortun.credmanback.controllers;
+
+import com.fortun.credmanback.models.entity.Credential;
+import com.fortun.credmanback.models.services.ICredentialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class CredentialRestController {
+
+    @Autowired
+    private ICredentialService credentialService;
+
+    @GetMapping("/credentials")
+    public List<Credential> index() {
+        return credentialService.findAll();
+    }
+
+    @GetMapping("/credentials/{id}")
+    public Credential show(@PathVariable Long id) {
+        return credentialService.findById(id);
+    }
+
+    @PostMapping("/credentials")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Credential create(@ModelAttribute Credential credential) {
+        this.credentialService.save(credential);
+        return credential;
+    }
+
+    @PutMapping("/credentials/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Credential update(@ModelAttribute Credential credential, @PathVariable Long id) {
+        Credential currentCredential = this.credentialService.findById(id);
+        currentCredential.setNameCredential(credential.getNameCredential());
+        currentCredential.setUserCredential(credential.getUserCredential());
+        currentCredential.setPasswordCredential(credential.getPasswordCredential());
+        currentCredential.setIdUserFK(credential.getIdUserFK());
+        this.credentialService.save(currentCredential);
+        return currentCredential;
+    }
+
+    @DeleteMapping("/credentials/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        this.credentialService.delete(id);
+    }
+}
