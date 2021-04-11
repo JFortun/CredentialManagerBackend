56edd79d0c95fc93134a5d5a50486d0344d3ccef
JFortun <josemanuelfortunromero@gmail.com>
2021-04-11 12:55:19 +0200

    Implemented Credential Service Interface

---
+++ src/main/java/com/fortun/credmanback/models/services/ICredentialService.java
@@ -0,0 +1,17 @@
+package com.fortun.credmanback.models.services;
+
+import com.fortun.credmanback.models.entity.Credential;
+
+import java.util.List;
+
+public interface ICredentialService {
+
+    List<Credential> findAll();
+
+    Credential save(Credential credential);
+
+    Credential findById(Long id);
+
+    void delete(Long id);
+
+}
